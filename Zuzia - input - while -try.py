09d223e2e4f61e249c80a9de7a9eb7b5cabe7891
tatarapawel <40491474+tatarapawel@users.noreply.github.com>
2018-12-28 14:16:52 +0100

    Programu dla Zuzi

---
+++ Zuzia - input - while -try.py
@@ -0,0 +1,12 @@
+imie=input("Podaj imie:")
+print("Twoje imie: %s" %imie)
+
+wprowadzono_Wiek=False
+
+while not wprowadzono_Wiek:
+    try:
+        ile_lat=int(input("Ile masz lat:"))
+        print("Masz lat: %s" %ile_lat)
+        wprowadzono_Wiek=True
+    except:
+        print("Wiek musi być liczbą całkowitą ")
