be4f46475f2468e161b19bd61b6ff6ccc44af397
tatarapawel <40491474+tatarapawel@users.noreply.github.com>
2018-12-29 15:18:02 +0100

    Pozycja gracza w Minecraft

---
+++ Ustaw_pozycje.py
@@ -0,0 +1,12 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+wprowadzone_pozycje=False
+while wprowadzone_pozycje==False:
+    try:
+        poz_x = int(input("Podaj pozycje x:"))
+        poz_y = int(input("Podaj pozycje y:"))
+        poz_z = int(input("Podaj pozycje z:"))
+        wprowadzone_pozycje=True
+    except:
+        print("Niepoprawna wartość. Wprowdź ponownie")
+mc.player.setPos(poz_x,poz_y,poz_z)
